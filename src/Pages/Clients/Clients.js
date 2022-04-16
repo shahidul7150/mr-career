f8c526eb171bd20c7a9a5e2013c72c14d80190de
Shahidul Islam <shahidulislam7150@gmail.com>
2022-04-16 23:40:22 +0600

    all page setup

---
+++ src/Pages/Clients/Clients.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Clients = () => {
+    return (
+        <div>
+            <h2>This is clients page</h2>
+        </div>
+    );
+};
+
+export default Clients;
---
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>This is notfound 404</h2>
+        </div>
+    );
+};
+
+export default NotFound;
