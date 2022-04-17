16d3ed0e01cd297ac551d754c2e13ea255ee8b13
Shahidul Islam <shahidulislam7150@gmail.com>
2022-04-17 16:52:38 +0600

    add blog and about me

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import myPic from "../../images/me.jpg"
 
 const About = () => {
     return (
-        <div>
-            <h2>This is about</h2>
+        <div className='p-5'>
+          
+            <img width="200px" src={myPic} alt="" />
+            <h2 className='text-white bg-primary w-25 mx-auto p-4'>Shahidul Islam</h2>
+            <p className='w-25 mx-auto text-start'>I would like to achieve my goal withing 6 months.So now I trying hard day and night in front of focus my goal .Everyday I would like to learning my confusing matter.I hope ,I over come my goal. </p>
         </div>
     );
 };
--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -10,7 +10,7 @@ const Blogs = () => {
                 <div>
                     <img width="600px" src="https://lh3.googleusercontent.com/16FwaRpvUmWs1ZfVVsYmoA4gqvNH8kazS9crLGTEZAxKKDuH6d_j_GKsqzt6WWwqHqgIJmmkYOu3Nk6QFotq5l7MdT-BpAOzoTJM8KX6ToS9SZCVIsj1v2wc5YZeg8twRuX34HER" alt="" />
 
-                    <h2 className='text-primary pt-3'>What is difference between  authentication and  authorization?</h2>
+                    <h2 className='w-50 mx-auto text-start text-primary pt-3'>What is difference between  authentication and  authorization?</h2>
                     <p className='w-50 mx-auto text-start'> <span className='fw-bold'>Authentication:</span>Authentication is verify user identity by credentials based on knowledge.User provide authentication factors when requesting access</p>
                     <p className='w-50 mx-auto text-start'> <span className='fw-bold'>Authorization:</span>Authorization permit to access all resource.Authorization identity and access control policies.By the authorization leadership sets security strategies.Network security staff implement and maintain access control system</p>
 
@@ -18,10 +18,32 @@ const Blogs = () => {
 <hr />
                 <div>
                     <img width="600px" src="https://firebase.google.com/images/products/auth/auth-3.png" alt="" />
-                    <h2 className='text-primary pt-3'>Why I'm using firebase and what other option I have to implement authentication?</h2>
+                    <h2 className='w-50 mx-auto text-start text-primary pt-3'>Why I'm using firebase and what other option I have to implement authentication?</h2>
                     <p className='w-50 mx-auto text-start'> <span className='fw-bold'>Why I'm using firebase: </span>Firebase is  provide authentication service.I using authentication system from firebase .It maintain by google and their securities system is very heigh .</p>
 
                     <p className='w-50 mx-auto text-start'> <span className='fw-bold'>What other option implement: </span> First of all I'm using firebase for authentication and then I using firebase for hosting my site .</p>
+                </div>
+                <div>
+                    <img width="600px" src="https://i.ytimg.com/vi/iosNuIdQoy8/maxresdefault.jpg" alt="" />
+                    <h2 className='w-50 mx-auto text-start text-primary pt-3'>What other services does firebase provide other than authentication?</h2>
+                    <div className='d-block'>
+                    <p className='w-50 mx-auto text-start'> <span className='fw-bold'>Build better apps: </span>
+                    <li className='list-unstyled '>Cloud Firebase</li>
+                    <li className='list-unstyled '>Firebase ML</li>
+                    <li className='list-unstyled '>Hosting</li>
+                    <li className='list-unstyled '>Cloud Firebase</li>
+                    <li className='list-unstyled '>Realtime Database</li>
+                    </p>
+
+                    <p className='w-50 mx-auto text-start'> <span className='fw-bold'>Improve app quality: </span>
+                    <li className='list-unstyled '>Crashlytics</li>
+                    <li className='list-unstyled '>Performance Monitoring</li>
+                    <li className='list-unstyled '>Test Tab</li>
+                    <li className='list-unstyled '>App Distribution</li>
+                   
+                    </p>
+                   </div>
+
                 </div>
             </div>
         </div>
