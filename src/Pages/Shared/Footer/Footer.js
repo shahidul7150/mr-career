8b34cac0df842cb777b3adbf7b8e252d73d1f604
Shahidul Islam <shahidulislam7150@gmail.com>
2022-04-17 11:35:51 +0600

    footer design

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Footer = () => {
+    const time = new Date()
+    const currentYear=time.getFullYear()
     return (
-        <div>
-            <h2>Footer</h2>
+        <div className='bg-primary p-2'>
+            <h5 className='text-white'>Mr.Career</h5>
+            <p className='text-white'><small>Alright reserved copy© {currentYear} </small></p>
         </div>
     );
 };
